7b44bebe0a12681aa00365acb9a281c0616c1d64
moscuet <mostifizur.rahman@integrify.iogit config --global user.email mostifizur.rahman@integrify.io>
2022-03-19 20:44:59 +0200

    search bar added at home

--- src/components/BooksComp.tsx
+++ src/components/BooksComp.tsx
@@ -1,26 +1,46 @@
 import React, { useState, useEffect } from 'react'
-import BookSearch from './searchBar/BookSearch'
-import userService from '../services/userService'
-import { Product } from '../types'
-//import {  AppState } from '../types'
-
-import BookCard from '../components/BookCard'
-
-import { AppState } from '../types'
-// const state = useSelector((state: AppState) => state)
 import {  useSelector } from 'react-redux'
 
+import userService from '../services/userService'
+import { Product } from '../types'
+import { AppState } from '../types'
+
+import BookCard from '../components/BookCard'
+import BookSearch from './searchBar/BookSearch'
+import Loader from 'react-ts-loaders'
 const BooksComp = () => {
-  //const dispatch = useDispatch()
   const cartProducts = useSelector((state: AppState) => state.order.inCart).map( p=>p._id)
 
   type ProductWithIncart = Product & { isIncart?: boolean }
+
+  const [loading, setLoading] = useState(false)
+  const [loadingSuccess, setLoadingSuccess] = useState(false)
+
+
   const [content, setContent] = useState<ProductWithIncart[]>([])
+  const [updateContent, setUpdatedContent] = useState<ProductWithIncart[]>([])
   const [message, setMessage] = useState('')
+  
+  type Handleseacrh = (word:string) =>void
+
+  const handleSearch:Handleseacrh =(searchword) =>{
+    console.log('search word $$$$$$',searchword, typeof(searchword))
+    const books = content.filter( book => {
+      console.log(book.authors.map( author=>author.firstName+ ' '+ author.lastName))
+      return book.title.toLowerCase().includes(searchword.toLowerCase())
+      || book.authors.map(author=>( author.firstName + ' '+ author.lastName)).toString().toLowerCase().includes(searchword.toLowerCase())
+      || book.genres.toString().toLowerCase().includes(searchword.toLowerCase())
+    })
+    setUpdatedContent(books)
+  }
+
 
   useEffect(() => {
+    setLoading(true)
     userService.getPublicContent().then(
       (response) => {
+        setLoading(false)
+        setLoadingSuccess(true)
         const updatedProduct: ProductWithIncart[] = response.data.map(
           (prod: ProductWithIncart) => {
             prod.isIncart = cartProducts.includes(prod._id)
@@ -28,8 +48,12 @@ const BooksComp = () => {
           }
         )
         setContent(updatedProduct)
+        setUpdatedContent(updatedProduct)
+
       },
       (error) => {
+        setLoading(false)
+        setLoadingSuccess(false)
         const _content =
           (error.response && error.response.data) ||
           error.message ||
@@ -39,23 +63,39 @@ const BooksComp = () => {
         console.log(error)
       }
     )
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   },[])
 
   console.log('content', content)
   console.log('message', message)
-
+  console.log(loading)
   return (
-    <div style={{ width: '100%' }}>
-      <div> <BookSearch /> </div>
-      <ul style={{ display: 'flex', flexWrap: 'wrap' }}>
-        {content.map((book) => (
-          <li key={book._id}>
-            <BookCard {...book} />
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      { 
+        loading && <div>
+          <Loader type="spinner" color="blue" />
+        </div>
+      }
+      {loadingSuccess && (
+        <div style={{ width: '100%' }}>
+          {
+            message?( <p>{message}</p>):
+              (<>
+                <div> <BookSearch handleSearch ={handleSearch} /> </div>
+                { updateContent.length===0? (<h1>No book found, change search word</h1>):
+                  <ul style={{ display: 'flex', flexWrap: 'wrap' }}>
+                    {updateContent.map((book) => (
+                      <li key={book._id}>
+                        <BookCard {...book} />
+                      </li>
+                    ))}
+                  </ul>}
+              </>
+              )
+          }
+        </div>
+      )}
+    </>
   )
 }
 export default BooksComp
--- src/components/searchBar/BookSearch.tsx
+++ src/components/searchBar/BookSearch.tsx
@@ -1,26 +1,29 @@
 import { Form } from 'react-bootstrap'
-import React, { ChangeEvent, useState } from 'react'
+import React, { ChangeEvent} from 'react'
+import './booksearch.css'
+type Handleseacrh = (word:string) =>void
 
-export default function BookSearc() {
-  const [value, setValue] = useState('')
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setValue(event.currentTarget.value)
-  }
+export default function BookSearc(props:{handleSearch:Handleseacrh}) {
+ 
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => props.handleSearch(event.currentTarget.value)
+  
   return (
     <div
-      style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}
+      className ='search-form-container' style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}
     >
-      <Form style={{ width: '350px' }}>
-        <Form.Group className="mb-3" controlId="searchInput">
-          {/* <Form.Label>Email address</Form.Label> */}
-          <Form.Control
-            type="text"
-            value = {value}
-            placeholder="book/author/catagory"
-            onChange={handleChange}
-          />
-        </Form.Group>
-      </Form>
+      <div><h3 style = { { marginRight:'20px', color:'green'}}>Pick your choice</h3></div>
+      <div>
+        <Form style={{ width: '350px' }}>
+          <Form.Group className="mb-3" controlId="searchInput">
+            {/* <Form.Label>Email address</Form.Label> */}
+            <Form.Control
+              type="text"
+              placeholder="search by book, author, catagory"
+              onChange={handleChange}
+            />
+          </Form.Group>
+        </Form>
+      </div>
     </div>
   )
 }
