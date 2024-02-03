ca2c393d0987625f5b8a290e5d5b1dfed9f3ff82
moscuet <mos.cuet@gmail.com>
2024-02-03 16:13:27 +0200

    cart scroll in small screen

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,4 +1,3 @@
-
 import { useSelector } from 'react-redux'
 import { AppState } from '../types'
 import { useDispatch } from 'react-redux'
@@ -12,39 +11,53 @@ function Cart() {
   const cartItems = useSelector((state: AppState) => state.order.inCart)
   const dispatch = useDispatch()
   return (
+    <div className="cart-wrapper">
+      <div className="cart">
+        <div>
+          <h5 className="cart_title"> Your Borrowing basket </h5>
+          {cartItems.length <= 0 && <div>No products in the cart</div>}
+          {cartItems.length > 0 && (
+            <div className="cart-list-wrapper">
+              <ul style={{ marginBottom: '40px' }}>
+                {cartItems.map((p) => (
+                  <li key={p._id}>
+                    <div>
+                      <Link to={`/books/${p._id}`}>{`${p.title}`}</Link>
+                    </div>
+                    <div>
+                      <Link to={`/books/${p._id}`}>
+                        {' '}
+                        <img src={`${p.img}`} alt="product img" />
+                      </Link>
+                    </div>
+                    <div>
+                      {`by ${p.authors?.[0]?.firstName} ${p.authors[0]?.lastName}, ${p.edition}`}
+                    </div>
+                    <div>
+                      <BUTTON
+                        style={{
+                          padding: '4px 8px',
+                          background: 'var(--button-danger-bg-color)',
+                        }}
+                        onClick={() => dispatch(removeProduct(p))}
+                      >
+                        Remove
+                      </BUTTON>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
 
-    <div className="cart" >
-      <div>
-        <h5 className="cart_title"> Your Borrowing basket </h5>
-        {cartItems.length <= 0 && <div>No products in the cart</div>}
-        {cartItems.length > 0 && <ul style={{ marginBottom: '40px' }}>
-          {cartItems.map((p) => (
-            <li key={p._id}>
-              <div>
-                <Link to={`/books/${p._id}`}>{`${p.title}`}</Link>
-              </div>
-              <div>
-                <Link to={`/books/${p._id}`}>
-                  {' '}
-                  <img src={`${p.img}`} alt="product img" />
-                </Link>
-              </div>
-              <div>
-                {`by ${p.authors?.[0]?.firstName} ${p.authors[0]?.lastName}, ${p.edition}`}
-              </div>
-              <div>
-              </div>
-              <div>
-                <BUTTON style={{ background: 'var(--button-danger-bg-color)' }} onClick={() => dispatch(removeProduct(p))}>Remove</BUTTON>
-              </div>
-            </li>
-          ))}
-        </ul>}
-
-        {cartItems.length > 0 && (<BUTTON ><Link to='/user/checkout'>Checkout</Link></BUTTON>)}
+          {cartItems.length > 0 && (
+            <BUTTON>
+              <Link to="/user/checkout">Checkout</Link>
+            </BUTTON>
+          )}
+        </div>
       </div>
     </div>
-
   )
 }
 
