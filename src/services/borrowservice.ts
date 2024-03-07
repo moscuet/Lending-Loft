55cfe272076d95db6334d5c26384c8ba0a1dd3f4
moscuet <mos.cuet@gmail.com>
2024-03-07 22:28:08 +0200

    typo

--- src/services/borrowservice.ts
+++ src/services/borrowservice.ts
@@ -1,41 +1,36 @@
-
-import axios from "axios";
-type Borrow ={
-    bookId: string[]
-    customerId: string[]
-    borrowDate?: Date
-    returnDate?: Date
-    isReturned?: Boolean
-    _id?:string
+import axios from 'axios'
+type Borrow = {
+  bookId: string[]
+  customerId: string[]
+  borrowDate?: Date
+  returnDate?: Date
+  isReturned?: Boolean
+  _id?: string
 }
 
 const API_URL = process.env.REACT_APP_API_URL
 
-
-class  borrowService {
+class borrowService {
   getAllBorrowList() {
-      throw new Error('Method not implemented.');
+    throw new Error('Method not implemented.')
   }
   deleteBorrow(_id: string) {
-    axios.delete(API_URL+ '/borrows/'+ _id)
-  }
-  
-  updateBorrow(borrow:Borrow){
-    console.log('borrow from borrowservice', borrow)
-    return axios.put(API_URL+ '/borrows/'+ borrow._id, borrow)
-  }
-  postBorrow(borrow:Borrow){
-    console.log('borrow from borrowservice', borrow)
-    return axios.post(API_URL+ '/borrows/', borrow)
-  }
-  getSingleBorrow(id:string){
-    return axios.get(API_URL+ '/borrows/'+id)
+    axios.delete(API_URL + '/borrows/' + _id)
   }
 
-
+  updateBorrow(borrow: Borrow) {
+    console.log('borrow from borrowservice', borrow)
+    return axios.put(API_URL + '/borrows/' + borrow._id, borrow)
+  }
+  postBorrow(borrow: Borrow) {
+    console.log('borrow from borrowservice', borrow)
+    return axios.post(API_URL + '/borrows/', borrow)
+  }
+  getSingleBorrow(id: string) {
+    return axios.get(API_URL + '/borrows/' + id)
+  }
 }
 
 const newborrowService = new borrowService()
 
 export default newborrowService
-
